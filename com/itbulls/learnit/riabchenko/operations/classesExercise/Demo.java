84fc127153b8cadf6e01265c9a7d3a2e0b1d4862
Oleksandr Riabchenko <sashaas86@gmail.com>
2021-01-03 11:52:41 +0200

    3

--- com/itbulls/learnit/riabchenko/operations/classesExercise/Demo.java
+++ com/itbulls/learnit/riabchenko/operations/classesExercise/Demo.java
@@ -5,6 +5,7 @@ import java.math.BigDecimal;
 public class Demo {
 
 	public static void main(String[] args) {
+		
 		BookService bookService = new BookService();
 		Book[] books = new Book[] {
 				new Book(1, "Book_1", new Author[] { new Author(1, "Jon", "Johnson") }, new Publisher(1, "Publisher_1"),
@@ -16,7 +17,7 @@ public class Demo {
 						new Publisher(1, "Publisher_1"), 1997, 350, BigDecimal.valueOf(34.99), CoverType.HARDCOVER),
 				new Book(4, "Book_4", new Author[] { new Author(4, "Craig", "Gregory") },
 						new Publisher(3, "Publisher_3"), 1992, 185, BigDecimal.valueOf(19.99), CoverType.PAPERBACK) };
-		
+
 		bookService.filterBooksByAuthor(new Author(2, "William", "Wilson"), books);
 		bookService.filterBooksByPublisher(new Publisher(1, "Publisher_1"), books);
 		bookService.filterBooksAfterSpecifiedYear(1992, books);
